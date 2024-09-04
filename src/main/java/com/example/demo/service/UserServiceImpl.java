f0ac07c6e53cc8a0ac86d5b3f2b141f33fcb85f1
mhemalatha <mhemalatha@mhemalatha-sa.stratapps.com>
2024-09-04 11:39:32 +0530

    changes related to new entities

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,16 +1,28 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
 @Service
 public class UserServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+	@Transactional
+	public User loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		return user;
 	}
+	
 
 }
